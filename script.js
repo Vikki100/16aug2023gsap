c92ff5030c424d7f1d842ce3d0b3480168fbf29f
Vikki100 <merasafar.13safar@gmail.com>
2023-08-16 04:57:13 +0530

    learing gsap animation

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// gsap.to('#box' ,{
+//     x:700,
+//     duration:1,
+//     delay:1,
+//     backgroundColor:"green",
+
+
+// })
+gsap.from('#box' ,{
+    x:700,
+    duration:1,
+    delay:1,
+    backgroundColor:"green",
+
+
+})
+gsap.from('#box1',{
+    scale:0,
+    duration:1,
+    opacity:0,
+    delay:1,
+    backgroundColor:"green",
+
+
+})
+
+// staggr
+// yoyo
